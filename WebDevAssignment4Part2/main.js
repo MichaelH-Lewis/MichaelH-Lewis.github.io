66c19ac6bb82e006e4f008493f73e44040635555
MichaelH-Lewis <throwawayemail22342@gmail.com>
2024-04-04 21:58:26 -0400

    added HTML header

---
+++ WebDevAssignment4Part2/main.js
@@ -0,0 +1,13 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Declaring the array of image filenames */
+
+/* Declaring the alternative text for each image file */
+
+/* Looping through images */
+
+/* Wiring up the Darken/Lighten button */
