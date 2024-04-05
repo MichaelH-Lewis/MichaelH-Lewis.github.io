389d04f857f154d8971bfe734112d4a78e0aa39c
MichaelH-Lewis <throwawayemail22342@gmail.com>
2024-04-04 20:59:11 -0400

    created my arrays with comments as well as added story text

--- WebDevAssignment4Part1/main.js
+++ WebDevAssignment4Part1/main.js
@@ -9,3 +9,12 @@ function randomValueFromArray (array) {
     const random = Math.floor(Math.random()* array.length);
     return array[random];
 }
+
+const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.'
+// An array that contains potential character names to be inserted into the story text
+const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
+// An array containting potential story locations to be inserted into the story text
+const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
+// An array containing potential actions to be inserted into the story text
+const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
+
