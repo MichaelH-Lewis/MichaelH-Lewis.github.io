d788d289f6fceeae7e981857785e4cbb636e4cd6
MichaelH-Lewis <throwawayemail22342@gmail.com>
2024-04-04 22:51:22 -0400

    created second function

--- WebDevAssignment4Part3/main.js
+++ WebDevAssignment4Part3/main.js
@@ -7,3 +7,7 @@ const height = (canvas.height = windowinnerHeight);
 function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+function randomRGB() {
+    return 'rgb(${random(0, 255)} $ {random(0, 255))})';
+}
