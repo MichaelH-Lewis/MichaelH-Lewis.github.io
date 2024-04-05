997e124e5c9ab55494ee03d496c579bb20c0feb8
MichaelH-Lewis <throwawayemail22342@gmail.com>
2024-04-04 22:08:53 -0400

    added image looping functionality and event listener

--- WebDevAssignment4Part2/main.js
+++ WebDevAssignment4Part2/main.js
@@ -9,5 +9,16 @@ const imageFileNames = ["pic1", "pic2", "pic3", "pic4", "pic5"]
 /* Declaring the alternative text for each image file */
 
 /* Looping through images */
+imageFileNames.forEach(filename => {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', xxx);
+    newImage.setAttribute('alt', xxx);
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener('click', function() {
+        displayedImage.src = this.src;
+        displayedImage.alt = this.alt;
+    });
+
+});
 
 /* Wiring up the Darken/Lighten button */
