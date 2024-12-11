e9af28bd8c45624be353f2d329880f735574270f
MichaelH-Lewis <156021124+MichaelH-Lewis@users.noreply.github.com>
2024-12-10 23:59:53 -0500

    Add files via upload
    committing for oop 2

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FlashlightApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿// Program Header
+// Program Author: Michael Lewis
+// Program Description: A basic flashlight application
+// Program Date: December 7th, 2024
+// Program Last Edit: December 10th, 2024
+
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.ApplicationModel;
+
+namespace FlashlightApp
+{
+    public partial class MainPage : ContentPage
+    {
+        // setting the flashlight off by default
+        private bool isFlashlightTurnedOn = false;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        // the functionality for my flashlight button
+        private async void FlashlightOn(object sender, EventArgs e)
+        {
+            isFlashlightTurnedOn = !isFlashlightTurnedOn;
+
+            // if the earlier stated bool flips to true, i.e. the user clicks taps the button, the flashlight turns on
+            if (isFlashlightTurnedOn)
+            {
+                // making use of the built-in MAUI flashlight functionality
+                await Flashlight.TurnOnAsync();
+            }
+            // otherwise it'll be off by default
+            else
+            {
+                await Flashlight.TurnOffAsync();
+            }
+
+           // setting the label text to correspond with the flashlight being turned on and off
+            StatusLabel.Text = isFlashlightTurnedOn ? "ON" : "OFF";
+        }
+    }
+}
